cd169ba80154cdb92225942a221ae0690afb2126
MG228 <mcgtech32@gmail.com>
2023-02-06 13:10:42 -0500

    reading the files.

--- LSP_S23/src/org/Howard/edu/hw2/WordCounting.java
+++ LSP_S23/src/org/Howard/edu/hw2/WordCounting.java
@@ -4,6 +4,21 @@
 
 package org.Howard.edu.hw2;
 
+import java.nio.file.*;;
+
+
 public class WordCounting {
+	public static String readFileAsString(String fileName) throws Exception
+	{
+		String data = "";
+		data = new String(Files.readAllBytes(Paths.get(null)));
+		return data;
+	}
+	
+	public static void main(String[] args) throws Exception
+	{ 
+		String data = readFileAsString("C:\\User\\maya3\\Desktop\\name");
+	}
+	
 
 }
